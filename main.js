29590cad45c216b4a380e3e97f7f4f07c01353dc
Ernesto Laval <ernesto.laval@gmail.com>
2017-09-24 23:41:49 +0100

    Use bootstrap & upgrade code

--- main.js
+++ main.js
@@ -1,3 +1,37 @@
 const randomNumber = Math.floor( Math.random() * 100);
 const targetElement = document.getElementById('luckyNumber');
 targetElement.textContent = randomNumber;
+
+
+initApp = function() {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+        // User is signed in.
+        var displayName = user.displayName;
+        var email = user.email;
+        var uid = user.uid;
+        user.getIdToken().then(function(accessToken) {
+            document.getElementById('sign-in-status').textContent =`Signed in - ${displayName || email}`;
+            document.getElementById('sign-in').textContent = 'Sign out';
+            document.getElementById('sign-in').addEventListener('click', function() {
+                firebase.auth().signOut();
+            });
+        })
+        } else {
+            // User is signed out.
+            document.getElementById('sign-in-status').textContent = 'Signed out';
+            document.getElementById('sign-in').textContent = 'Sign in';
+            document.getElementById('account-details').textContent = '';
+            document.getElementById('sign-in').addEventListener('click', () => {
+                window.location.assign('/login.html');
+            });
+        }
+    }, function(error) {
+        console.log(error);
+    });
+};
+
+window.addEventListener('load', function() {
+    initApp()
+});
+
